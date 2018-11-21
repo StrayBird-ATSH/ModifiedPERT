c3bc204970f38bb4e6a733757abab393baddbe46
Wang Chen <zhanghuilian-98022@163.com>
2018-11-21 19:51:42 +0800

    Refactored on method

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,66 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Graph {
     Vertex[] vertices;
 
     Graph(int vertexNumber) {
         vertices = new Vertex[vertexNumber];
+    }
+
+    void PERT() {
+        ArrayList<Vertex> sort = topologicalSort();
+        initializeSingleSource();
+        for (Vertex u : sort)
+            for (Vertex v : u.adj)
+                relax(u, v);
+    }
+
+    private void relax(@NotNull Vertex u, @NotNull Vertex v) {
+        if (v.d < u.d + v.weight) {
+            v.d = u.d + v.weight;
+            v.pi = u;
+        }
+    }
+
+    private void initializeSingleSource() {
+        for (Vertex v : vertices) {
+            v.d = v.weight;
+            v.pi = null;
+        }
+    }
+
+    private ArrayList<Vertex> topologicalSort() {
+        DFS();
+        ArrayList<Vertex> sortedV = (ArrayList<Vertex>) Arrays.asList(vertices);
+        sortedV.sort(new Vertex.FinishTimeComparator());
+        return sortedV;
+    }
+
+    private void DFS() {
+        for (Vertex u : vertices) {
+            u.color = Color.WHITE;
+            u.pi = null;
+        }
+        int time = 0;
+        for (Vertex u : vertices)
+            if (u.color.equals(Color.WHITE))
+                DFSVisit(u, time);
+    }
+
+    private void DFSVisit(@NotNull Vertex u, int time) {
+        time++;
+        u.d = time;
+        u.color = Color.GREY;
+        for (Vertex v : u.adj)
+            if (v.color.equals(Color.WHITE)) {
+                v.pi = u;
+                DFSVisit(u, time);
+            }
+        u.color = Color.BLACK;
+        time++;
+        u.f = time;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -25,62 +21,14 @@ public class Main {
             String weight = input.nextLine();
             for (int i = 1; i <= m; i++)
                 graph.vertices[i].weight = Integer.parseInt(weight.split(" ")[i - 1]);
+            graph.PERT();
+            int distance = 0;
+            for (Vertex vertex : graph.vertices)
+                if (vertex.d > distance)
+                    distance = vertex.d;
+            System.out.println(distance);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-    }
-
-    private void PERT(Graph G) {
-        ArrayList<Vertex> sort = topologicalSort(G);
-        initializeSingleSource(G);
-        for (Vertex u : sort)
-            for (Vertex v : u.adj)
-                relax(u, v);
-    }
-
-    private void relax(@NotNull Vertex u, @NotNull Vertex v) {
-        if (v.d < u.d + v.weight) {
-            v.d = u.d + v.weight;
-            v.pi = u;
-        }
-    }
-
-    private void initializeSingleSource(@NotNull Graph G) {
-        for (Vertex v : G.vertices) {
-            v.d = v.weight;
-            v.pi = null;
-        }
-    }
-
-    private ArrayList<Vertex> topologicalSort(Graph G) {
-        DFS(G);
-        ArrayList<Vertex> sortedV = (ArrayList<Vertex>) Arrays.asList(G.vertices);
-        sortedV.sort(new Vertex.FinishTimeComparator());
-        return sortedV;
-    }
-
-    private void DFS(@NotNull Graph G) {
-        for (Vertex u : G.vertices) {
-            u.color = Color.WHITE;
-            u.pi = null;
-        }
-        int time = 0;
-        for (Vertex u : G.vertices)
-            if (u.color.equals(Color.WHITE))
-                DFSVisit(u, time);
-    }
-
-    private void DFSVisit(@NotNull Vertex u, int time) {
-        time++;
-        u.d = time;
-        u.color = Color.GREY;
-        for (Vertex v : u.adj)
-            if (v.color.equals(Color.WHITE)) {
-                v.pi = u;
-                DFSVisit(u, time);
-            }
-        u.color = Color.BLACK;
-        time++;
-        u.f = time;
     }
 }
